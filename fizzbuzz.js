b0620bbbe9b4e119c81b121439371cbfc70bd3d6
jeannebardin <jmbardin@gmail.com>
2022-06-12 13:40:08 +1200

    release 4

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,7 +3,7 @@ const arr2 = [12, 22, 45, 47, 15, 63, 7]
 const arr3 = [17, -12, 'hello', 5, 23, 60, -7, null, 13]
 
 ​
-function fizzBuzz(apple) {
+function fizzbuzz(apple) {
   for (let i = 0; i < apple.length; i++) { 
     let element = apple[i] 
     if (element % 3 === 0 && element % 5 === 0) {
@@ -17,8 +17,11 @@ function fizzBuzz(apple) {
     }
   }
 }
-​
-fizzBuzz(arr2)
+
+fizzbuzz(arr2)
+
+const result = fizzbuzz(arr1)
+console.log(result)
 ​
 
 
